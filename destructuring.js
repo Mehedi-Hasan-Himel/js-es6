4a20afad04f1ef42e9b482bc91a5ab308219387c
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-10-11 22:31:09 +0600

    first commit

---
+++ destructuring.js
@@ -0,0 +1,15 @@
+const actor = {
+  name: "Ananta",
+  age: 30,
+  phone: "01969038472",
+};
+
+const phone = actor.phone;
+// console.log(phone);
+
+// array destructuring
+const numbers = [1, 2];
+const [first, second] = numbers;
+// console.log(first, second);
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const actor = {
+  name: "Ananta",
+  age: 30,
+  phone: "01969038472",
+};
+
+const phone = actor.phone;
+console.log(phone);
