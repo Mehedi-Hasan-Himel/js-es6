f54e258e464934b9f6b5c87f6cae8e97ced33255
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-11-16 18:58:23 +0600

    foreach, filter, find, and differences between them

---
+++ filter.js
@@ -0,0 +1,15 @@
+const numbers = [1, 5, 6, 4, 15];
+const players = [75, 65, 67, 72, 55, 59];
+
+/* const selected = players.filter((p) => p > 60 );
+const selected = players.filter((p) => p > 50 );
+const selected = players.filter((p) => p > 80 ); */
+// console.log(selected);
+
+// const odd = players.filter((p) => p % 2 == 1);
+// console.log(odd);
+
+
+// const even = players.filter((p) => p % 2 == 0);
+// console.log(even);
+
