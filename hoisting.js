014b6324427176f85e6f9704f9b17de89bd77a94
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-11-22 20:36:48 +0600

    scope & hoiesting

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+// print5();
+// print10();
+
+
+for (var i = 0; i < 5; i++) {
+  //   console.log(i);
+}
+
+// console.log("Outside", i);
+
+function print5() {
+  console.log("inside print5", 5);
+}
+
+const print10 = function () {
+  console.log("inside print10", 10);
+};
